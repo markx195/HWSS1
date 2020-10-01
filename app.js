9c1604bff9ff9449e1bd3b470b9d0ed3cf4ae46f
markx195 <npqa195.mark@gmail.com>
2020-10-01 16:51:51 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let r = parseInt(prompt("Radius? "));
+let s = "Area"; 
+let sHinhTron = (r*r)*3.14159265358979
+console.log(s +"= "+  sHinhTron);
+document.write(sHinhTron);
+
---
+++ app2.js
@@ -0,0 +1,5 @@
+let a = parseInt(prompt("Enter the temperature in Celsius ? "));
+let f = (1.8*a)+32
+console.log(a + "(C) = " + f + "(F)");
+
+
